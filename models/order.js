00efa95fe5fe898abcb239e5af6f3baad86612f7
Leo <lbajdull@gmail.com>
2019-06-07 20:55:00 +0300

    added nodemon package + tried to implement jwt authentication

--- models/order.js
+++ models/order.js
@@ -4,10 +4,10 @@ const Schema = mongoose.Schema;
 const orderSchema = Schema({
     id: Schema.Types.ObjectId,
     user: {type: Schema.Types.ObjectId, ref: 'User'},
-    services: [{type: Schema.Types.ObjectId, ref: 'Service'}]
+    services: [Object]
 }, {
     collection: 'orders'
 });
 const Order = mongoose.model('Order', orderSchema);
 
-export default Order;
+module.exports = Order;
